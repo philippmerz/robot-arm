4bf9759dc26cdef8d6fcb838c43e46c50e5ba53a
philippmerz <philippmerz48@gmail.com>
2025-02-27 09:23:13 +0100

    Init

---
+++ robotarm_student_2021a_Ebox_ert_rtw/codertarget_assembly_flags.mk
@@ -0,0 +1,5 @@
+ASFLAGS_ADDITIONAL = 
+TARGET_LOAD_CMD = 
+TARGET_LOAD_CMD_ARGS = 
+TARGET_PKG_INSTALLDIR = /Users/philipp/Documents/MATLAB/SupportPackages/R2021a/toolbox/realtime/targets/raspi
+STACK_SIZE = 64
